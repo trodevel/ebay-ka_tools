060fbb20d23158913829c2d9b3184aadd5994687
trodevel <trodevel@gmail.com>
2022-05-30 18:22:28 +0200

    intermediate results

--- get_photos.sh
+++ get_photos.sh
@@ -4,14 +4,14 @@ LINK=$1
 FOLDER=$2
 
 [[ -z $LINK ]]   && echo "ERROR: no URL is given" && exit 1
-[[ -z $FOLDER ]] && echo "ERROR: no FOLDER is given" && exit 1
+[[ -z $FOLDER ]] && echo "ERROR: no output FOLDER is given" && exit 1
 
 [[ ! -d $FOLDER ]] && mkdir $FOLDER
 
 AGENT="user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
 
 datum=$( date -u +"%Y%m%d_%H%M%S" )
-PAGE=get_photos_page_${datum}_${RANDOM}
+PAGE=$FOLDER/get_photos_page_${datum}_${RANDOM}
 
 curl -H "$AGENT" "$LINK" > $PAGE
 
@@ -21,7 +21,7 @@ i=1
 
 for s in $links
 do
-    wget $s -O $i.jpg
+    wget $s -O $FOLDER/$i.jpg
     i=$((i+1))
 done
 

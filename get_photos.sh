53810fbc9984a493dab64756e9649270d943ca02
trodevel <trodevel@gmail.com>
2022-05-30 18:20:42 +0200

    added param

--- get_photos.sh
+++ get_photos.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 LINK=$1
+FOLDER=$2
 
-[[ -z $LINK ]] && echo "ERROR: no URL is given" && exit 1
+[[ -z $LINK ]]   && echo "ERROR: no URL is given" && exit 1
+[[ -z $FOLDER ]] && echo "ERROR: no FOLDER is given" && exit 1
+
+[[ ! -d $FOLDER ]] && mkdir $FOLDER
 
 AGENT="user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
 

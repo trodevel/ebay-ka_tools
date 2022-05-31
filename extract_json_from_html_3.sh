21652376ab503d6615947f4735dec4f5fd5f1244
trodevel <trodevel@gmail.com>
2022-05-31 19:34:51 +0200

    added extract_json_from_html_3.sh

---
+++ extract_json_from_html_3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INP=$1
+OUTP=$2
+
+[[ -z $INP ]]   && echo "ERROR: INP is not given" && exit 1
+[[ -z $OUTP ]]  && echo "ERROR: OUTP is not given" && exit 1
+
+[[ ! -f $INP ]] && echo "ERROR: file $INP not found" && exit 1
+
+awk '/Belen.Search.ViewAdView.init\({/,/}\);/' $INP | sed -e "s/Belen.Search.ViewAdView.init(//" -e "s/});/}/" | grep -v "adImageUrl" | sed 's/\(\w+\):/"\1"/' | jq . > $OUTP
